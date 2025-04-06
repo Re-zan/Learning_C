0c791715a8ecd889b87f7e448c4b0c9d16ac5fb7
Re-zan <zannat881all@gmail.com>
2025-04-06 15:18:23 +0600

    files are added

---
+++ bool.c
@@ -0,0 +1,10 @@
+#include<stdbool.h>
+#include<stdio.h>
+int main(){
+
+        bool a;
+        scanf("%d",&a);
+        bool y =false ;
+        printf("%d\n%d",a, y) ;
+    
+}
---
+++ first.c
@@ -0,0 +1,7 @@
+
+#include<stdio.h>
+int main(){
+    printf("hello zannatn please be serious");
+    return 0;
+   
+}
